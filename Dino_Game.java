d34ef9845187c4cadfd19ef49b38df6ef55e7f22
DeJarnetteI <105015032+DeJarnetteI@users.noreply.github.com>
2022-05-05 12:26:25 -0700

    Add files via upload

---
+++ Dino_Game.java
@@ -0,0 +1,38 @@
+import com.badlogic.gdx.ApplicationAdapter; 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.*;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer; 
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Rectangle; 
+import com.badlogic.gdx.math.Circle; 
+import com.badlogic.gdx.Input.Keys; 
+import com.badlogic.gdx.math.Vector2; 
+import com.badlogic.gdx.math.MathUtils; 
+import com.badlogic.gdx.math.Intersector; 
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.Texture; 
+import com.badlogic.gdx.InputProcessor; 
+import com.badlogic.gdx.*; 
+import com.badlogic.gdx.utils.Array; 
+import chn.util.*;
+import apcslib.*;
+
+public class Dino_Game extends ApplicationAdapter
+{
+    private Rectangle cacti;
+    private Rectangle dino;
+    
+    @Override
+    public void create(){
+        
+    }
+    
+    
+    
+}
---
+++ Game_Launcher.java
@@ -0,0 +1,6 @@
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+public class Game_Launcher
+{
+    
+}
