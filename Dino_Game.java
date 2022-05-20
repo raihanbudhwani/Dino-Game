2245c65f19f9f9106e7ac9e40d3da8a8dccb6e27
raihanbudhwani <77563672+raihanbudhwani@users.noreply.github.com>
2022-05-20 12:45:30 -0700

    Update Dino_Game.java

--- Dino_Game.java
+++ Dino_Game.java
@@ -21,12 +21,18 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.*; 
 import com.badlogic.gdx.utils.Array;
 import java.util.*;
-//import com.badlogic.gdx.audio;
+import com.badlogic.gdx.audio.*;
+
+//FIX THESE THINGS!!!!!!
+//dino step
+//clouds
+//explosion easter egg
+//sounds --> explosion and jump and dead
+//bird generation
 
 public class Dino_Game extends ApplicationAdapter
 {
     //instance variables
-
     private float yVel; 
     private float timer;
     private float highScore;
@@ -41,6 +47,8 @@ public class Dino_Game extends ApplicationAdapter
     private Rectangle rBird;
     private Rectangle dino;
     private Rectangle ground;
+    private Rectangle ground2;
+    private Rectangle ground3;
     private Rectangle hitBox;
     private Rectangle rMeteor;
 
@@ -54,9 +62,13 @@ public class Dino_Game extends ApplicationAdapter
     private Texture tCactus4;
     private Texture tCactus5;
     private Texture tGround;
+    private Texture tGround2;
+    private Texture tGround3;
     private Texture sun;
     private Texture tBird;
     private Texture background;
+    private Texture background2;
+    private Texture background3;
     private Texture tMeteor;
     private Texture explosion;
 
@@ -87,6 +99,8 @@ public class Dino_Game extends ApplicationAdapter
         tCactus4 = new Texture(Gdx.files.internal("Cactus-4.png"));
         tCactus5 = new Texture(Gdx.files.internal("Cactus-5.png"));
         tGround = new Texture(Gdx.files.internal("Ground.png"));
+        tGround2 = new Texture(Gdx.files.internal("Ground.png"));
+        tGround3 = new Texture(Gdx.files.internal("Ground.png"));
         sun = new Texture(Gdx.files.internal("Sun.png"));
         background = new Texture(Gdx.files.internal("background.png"));
         tBird = new Texture(Gdx.files.internal("bird.png"));
@@ -118,6 +132,8 @@ public class Dino_Game extends ApplicationAdapter
         rBird = new Rectangle(WORLD_WIDTH+3000, WORLD_HEIGHT-150, WORLD_WIDTH/10, WORLD_HEIGHT/7);
         dino = new Rectangle(WORLD_WIDTH-600, WORLD_HEIGHT-210, WORLD_WIDTH/12-10, WORLD_HEIGHT/7);
         ground = new Rectangle(0, 135, WORLD_WIDTH, WORLD_HEIGHT/9);
+        ground2 = new Rectangle(WORLD_WIDTH, 135, WORLD_WIDTH, WORLD_HEIGHT/9);
+        ground3 = new Rectangle(WORLD_WIDTH*2, 135, WORLD_WIDTH, WORLD_HEIGHT/9);
     }
 
     //renderer
@@ -165,11 +181,14 @@ public class Dino_Game extends ApplicationAdapter
             batch.begin();
             batch.draw(background, 0, 0, WORLD_WIDTH, WORLD_HEIGHT);//background
             batch.draw(tGround, 0, 135, WORLD_WIDTH, WORLD_HEIGHT/9);//ground
+            batch.draw(tGround2, WORLD_WIDTH, 135, WORLD_WIDTH, WORLD_HEIGHT/9);
+            batch.draw(tGround3, WORLD_WIDTH*2, 135, WORLD_WIDTH, WORLD_HEIGHT/9);
             batch.draw(sun, 570, 270, WORLD_WIDTH/5, WORLD_HEIGHT/3); //sun
             batch.draw(deadDino, dino.x, dino.y, dino.width, dino.height);
             font.draw(batch, layout, WORLD_WIDTH/2 - layout.width/2, WORLD_HEIGHT/2 + layout.height/2);
             font.draw(batch, layout2, WORLD_WIDTH-43 - layout.width/2, WORLD_HEIGHT-50 + layout.height/2);
             batch.end();
+
             timer = 0;
             if(Gdx.input.isKeyJustPressed(Keys.ENTER)){
                 gamestate = GameState.GAME;
@@ -193,67 +212,71 @@ public class Dino_Game extends ApplicationAdapter
 
         if(gamestate == GameState.EASTEREGG){
             layout.setText(font, "HI    " + (int)highScore + "    " + (int)timer);
-            batch.begin();
-            font.draw(batch, layout, WORLD_WIDTH-200 - layout.width/2, WORLD_HEIGHT-50 + layout.height/2); //change this later
-            batch.end();
-            layout.setText(font, "Press ENTER to Play Again or ESCAPE to go to the Menu");
+            layout2.setText(font, "Press ENTER to Play Again or ESCAPE to go to the Menu");
             if(Gdx.input.isKeyJustPressed(Keys.ENTER)){
                 gamestate = GameState.GAME;
             }
             if(Gdx.input.isKeyJustPressed(Keys.ESCAPE)){
                 gamestate = GameState.MENU;
             }
+
             batch.begin();
             batch.draw(background, 0, 0, WORLD_WIDTH, WORLD_HEIGHT);//background
-            batch.draw(tGround, ground.x, ground.y, ground.width, ground.height);//ground
+            batch.draw(tGround, 0, 135, WORLD_WIDTH, WORLD_HEIGHT/9);//ground
+            batch.draw(tGround2, WORLD_WIDTH, 135, WORLD_WIDTH, WORLD_HEIGHT/9);
+            batch.draw(tGround3, WORLD_WIDTH*2, 135, WORLD_WIDTH, WORLD_HEIGHT/9);
             batch.draw(sun, 570, 270, WORLD_WIDTH/5, WORLD_HEIGHT/3); //sun
             batch.draw(deadDino, dino.x, dino.y, dino.width, dino.height);
             batch.draw(tMeteor, rMeteor.x, rMeteor.y, rMeteor.width, rMeteor.height);
             batch.end();
-
             rMeteor.x -= 10;
             rMeteor.y -= 10;
 
-            if(rMeteor.overlaps(ground)){
-                rMeteor.x = 0;
-                rMeteor.y = 140;                
+            if(rMeteor.overlaps(dino)){
+                rMeteor.x = WORLD_WIDTH-600;
+                rMeteor.y = WORLD_HEIGHT-210;
                 batch.begin();
-                batch.draw(tMeteor, 0, 0, 0, 0);
-                batch.draw(explosion, WORLD_WIDTH-425, WORLD_HEIGHT-210, 200, 200);
-                font.draw(batch, layout, WORLD_WIDTH/2 - layout.width/2, WORLD_HEIGHT/2 + layout.height/2);
+                batch.draw(explosion, 200, 135, 200, 200);
                 batch.end();
             }
+            batch.begin();
+            font.draw(batch, layout, WORLD_WIDTH-200 - layout.width/2, WORLD_HEIGHT-50 + layout.height/2);
+            font.draw(batch, layout2, WORLD_WIDTH/2 - layout2.width/2, ((WORLD_HEIGHT/3)+10) + layout.height/2);
+            batch.end();
         }
 
         if(gamestate == GameState.GAME)
         {     
-            timer += 10;
+            timer += 0.03;
             layout.setText(font, "HI    " + (int)highScore + "    " + (int)timer);
             if(Gdx.input.isKeyJustPressed(Keys.SPACE))
             {
-                batch.begin();
-                batch.draw(dinoNorm, dino.x, dino.y, dino.width, dino.height);
-                batch.end();
+                // batch.begin();
+                // batch.draw(dinoNorm, dino.x, dino.y, dino.width, dino.height);
+                // batch.end();
                 if(dino.y == maxHeight){
                     yVel = 10;
                 }
-            } 
+            }
+
             yVel -= GRAVITY; 
             dino.y += yVel;        
             if(dino.y < maxHeight){//don't fall through the ground
                 dino.y = maxHeight;
             }
-            
+
             batch.begin();
             batch.draw(background, 0, 0, WORLD_WIDTH, WORLD_HEIGHT);//background
             batch.draw(tGround, ground.x, ground.y, ground.width, ground.height);//ground
+            batch.draw(tGround2, ground2.x, ground2.y, ground2.width, ground2.height);
+            batch.draw(tGround3, ground3.x, ground3.y, ground3.width, ground3.height);
+            batch.draw(dinoNorm, dino.x, dino.y, dino.width, dino.height);//dino
             batch.draw(sun, 570, 270, WORLD_WIDTH/5, WORLD_HEIGHT/3); //sun
-            batch.draw(dinoNorm, dino.x, dino.y, dino.width, dino.height);
 
             font.draw(batch, layout, WORLD_WIDTH-200 - layout.width/2, WORLD_HEIGHT-50 + layout.height/2); //change this later
 
-            //batch.draw(leftFootDino, dino.x, dino.y, dino.width, dino.height);
-            //batch.draw(rightFootDino, dino.x, dino.y, dino.width, dino.height);
+            // batch.draw(leftFootDino, dino.x, dino.y, dino.width, dino.height);
+            // batch.draw(rightFootDino, dino.x, dino.y, dino.width, dino.height);
 
             batch.draw(tCactus1, rCactus1.x, rCactus1.y, rCactus1.width, rCactus1.height);
             batch.draw(tCactus2, rCactus2.x, rCactus2.y, rCactus2.width, rCactus2.height);
@@ -264,7 +287,7 @@ public class Dino_Game extends ApplicationAdapter
 
             batch.end();
 
-            if(timer < 50){
+            if(timer < 10){
                 cactusSpeed = 6;
                 rCactus1.x -= cactusSpeed;
                 rCactus2.x -= cactusSpeed;
@@ -273,11 +296,55 @@ public class Dino_Game extends ApplicationAdapter
                 rCactus5.x -= cactusSpeed;
                 rBird.x -= cactusSpeed;
                 ground.x -= cactusSpeed;
-                if(ground.x < 0)
-                    ground.x = WORLD_WIDTH;
+                ground2.x -= cactusSpeed;
+                ground3.x -= cactusSpeed;
+                if(ground.x+(WORLD_WIDTH-10) <= 0)
+                    ground.x = WORLD_WIDTH*2;
+                if(ground2.x+(WORLD_WIDTH-10) < 0)
+                    ground2.x = WORLD_WIDTH*2;
+                if(ground3.x+(WORLD_WIDTH-10) < 0)
+                    ground3.x = WORLD_WIDTH*2;
             }
 
-            if(timer >= 50 && timer < 100){
+            if(timer >= 10 && timer < 20){
+                cactusSpeed = 7;
+                rCactus1.x -= cactusSpeed;
+                rCactus2.x -= cactusSpeed;
+                rCactus3.x -= cactusSpeed;
+                rCactus4.x -= cactusSpeed;
+                rCactus5.x -= cactusSpeed;
+                rBird.x -= cactusSpeed;
+                ground.x -= cactusSpeed;
+                ground2.x -= cactusSpeed;
+                ground3.x -= cactusSpeed;
+                if(ground.x+(WORLD_WIDTH-10) <= 0)
+                    ground.x = WORLD_WIDTH*2;
+                if(ground2.x+(WORLD_WIDTH-10) < 0)
+                    ground2.x = WORLD_WIDTH*2;
+                if(ground3.x+(WORLD_WIDTH-10) < 0)
+                    ground3.x = WORLD_WIDTH*2;
+            }
+
+            if(timer >= 20 && timer < 30){
+                cactusSpeed = 8;
+                rCactus1.x -= cactusSpeed;
+                rCactus2.x -= cactusSpeed;
+                rCactus3.x -= cactusSpeed;
+                rCactus4.x -= cactusSpeed;
+                rCactus5.x -= cactusSpeed;
+                rBird.x -= cactusSpeed;
+                ground.x -= cactusSpeed;
+                ground2.x -= cactusSpeed;
+                ground3.x -= cactusSpeed;
+                if(ground.x+(WORLD_WIDTH-10) <= 0)
+                    ground.x = WORLD_WIDTH*2;
+                if(ground2.x+(WORLD_WIDTH-10) < 0)
+                    ground2.x = WORLD_WIDTH*2;
+                if(ground3.x+(WORLD_WIDTH-10) < 0)
+                    ground3.x = WORLD_WIDTH*2;
+            }
+            
+            if(timer >= 30 && timer < 40){
                 cactusSpeed = 9;
                 rCactus1.x -= cactusSpeed;
                 rCactus2.x -= cactusSpeed;
@@ -286,14 +353,55 @@ public class Dino_Game extends ApplicationAdapter
                 rCactus5.x -= cactusSpeed;
                 rBird.x -= cactusSpeed;
                 ground.x -= cactusSpeed;
-                if(ground.x < 0)
-                    ground.x = WORLD_WIDTH;
+                ground2.x -= cactusSpeed;
+                ground3.x -= cactusSpeed;
+                if(ground.x+(WORLD_WIDTH-10) <= 0)
+                    ground.x = WORLD_WIDTH*2;
+                if(ground2.x+(WORLD_WIDTH-10) < 0)
+                    ground2.x = WORLD_WIDTH*2;
+                if(ground3.x+(WORLD_WIDTH-10) < 0)
+                    ground3.x = WORLD_WIDTH*2;
             }
-            //add while loop for ground thingy
-            //add multiple backgrounds so that it can reset without error
-            //try both options to see if they'll work
-
-            if(timer >= 100){
+            
+            if(timer >= 40 && timer < 50){
+                cactusSpeed = 10;
+                rCactus1.x -= cactusSpeed;
+                rCactus2.x -= cactusSpeed;
+                rCactus3.x -= cactusSpeed;
+                rCactus4.x -= cactusSpeed;
+                rCactus5.x -= cactusSpeed;
+                rBird.x -= cactusSpeed;
+                ground.x -= cactusSpeed;
+                ground2.x -= cactusSpeed;
+                ground3.x -= cactusSpeed;
+                if(ground.x+(WORLD_WIDTH-10) <= 0)
+                    ground.x = WORLD_WIDTH*2;
+                if(ground2.x+(WORLD_WIDTH-10) < 0)
+                    ground2.x = WORLD_WIDTH*2;
+                if(ground3.x+(WORLD_WIDTH-10) < 0)
+                    ground3.x = WORLD_WIDTH*2;
+            }
+            
+            if(timer >= 50 && timer < 60){
+                cactusSpeed = 11;
+                rCactus1.x -= cactusSpeed;
+                rCactus2.x -= cactusSpeed;
+                rCactus3.x -= cactusSpeed;
+                rCactus4.x -= cactusSpeed;
+                rCactus5.x -= cactusSpeed;
+                rBird.x -= cactusSpeed;
+                ground.x -= cactusSpeed;
+                ground2.x -= cactusSpeed;
+                ground3.x -= cactusSpeed;
+                if(ground.x+(WORLD_WIDTH-10) <= 0)
+                    ground.x = WORLD_WIDTH*2;
+                if(ground2.x+(WORLD_WIDTH-10) < 0)
+                    ground2.x = WORLD_WIDTH*2;
+                if(ground3.x+(WORLD_WIDTH-10) < 0)
+                    ground3.x = WORLD_WIDTH*2;
+            }
+            
+            if(timer >= 60){
                 cactusSpeed = 12;
                 rCactus1.x -= cactusSpeed;
                 rCactus2.x -= cactusSpeed;
@@ -302,8 +410,14 @@ public class Dino_Game extends ApplicationAdapter
                 rCactus5.x -= cactusSpeed;
                 rBird.x -= cactusSpeed;
                 ground.x -= cactusSpeed;
-                if(ground.x < 0)
-                    ground.x = WORLD_WIDTH;
+                ground2.x -= cactusSpeed;
+                ground3.x -= cactusSpeed;
+                if(ground.x+(WORLD_WIDTH-10) <= 0)
+                    ground.x = WORLD_WIDTH*2;
+                if(ground2.x+(WORLD_WIDTH-10) < 0)
+                    ground2.x = WORLD_WIDTH*2;
+                if(ground3.x+(WORLD_WIDTH-10) < 0)
+                    ground3.x = WORLD_WIDTH*2;
             }
 
             if(rCactus1.x < 0){
@@ -336,46 +450,39 @@ public class Dino_Game extends ApplicationAdapter
                 rBird.x = WORLD_WIDTH+rand;
             }
 
-            if(rCactus1.x + 75 >= rCactus2.x || rCactus1.x + 75 >= rCactus3.x || rCactus1.x + 75 >= rCactus4.x || rCactus1.x + 75 >= rCactus5.x
-            || rCactus1.x - 75 >= rCactus2.x || rCactus1.x - 75 >= rCactus3.x || rCactus1.x - 75 >= rCactus4.x || rCactus1.x - 75 >= rCactus5.x)
+            if(rCactus1.x + 50 >= rCactus2.x || rCactus1.x + 50 >= rCactus3.x || rCactus1.x + 50 >= rCactus4.x || rCactus1.x + 50 >= rCactus5.x
+            || rCactus1.x - 50 >= rCactus2.x || rCactus1.x - 50 >= rCactus3.x || rCactus1.x - 50 >= rCactus4.x || rCactus1.x - 50 >= rCactus5.x)
             {
                 int rand = (int)(Math.random()*4000+200);
                 rCactus1.x = WORLD_WIDTH+rand;
             }
 
-            if(rCactus2.x + 75 >= rCactus1.x || rCactus2.x + 75 >= rCactus3.x || rCactus2.x + 75 >= rCactus4.x || rCactus2.x + 75 >= rCactus5.x
-            || rCactus2.x - 75 >= rCactus1.x || rCactus2.x - 75 >= rCactus3.x || rCactus2.x - 75 >= rCactus4.x || rCactus2.x - 75 >= rCactus5.x)
+            if(rCactus2.x + 50 >= rCactus1.x || rCactus2.x + 50 >= rCactus3.x || rCactus2.x + 50 >= rCactus4.x || rCactus2.x + 50 >= rCactus5.x
+            || rCactus2.x - 50 >= rCactus1.x || rCactus2.x - 50 >= rCactus3.x || rCactus2.x - 50 >= rCactus4.x || rCactus2.x - 50 >= rCactus5.x)
             {
                 int rand = (int)(Math.random()*4000+200);
                 rCactus2.x = WORLD_WIDTH+rand;
             }
 
-            if(rCactus3.x + 75 >= rCactus2.x || rCactus3.x + 75 >= rCactus1.x || rCactus3.x + 75 >= rCactus4.x || rCactus3.x + 75 >= rCactus5.x
-            || rCactus3.x - 75 >= rCactus2.x || rCactus3.x - 75 >= rCactus1.x || rCactus3.x - 75 >= rCactus4.x || rCactus3.x - 75 >= rCactus5.x)
+            if(rCactus3.x + 50 >= rCactus2.x || rCactus3.x + 50 >= rCactus1.x || rCactus3.x + 50 >= rCactus4.x || rCactus3.x + 50 >= rCactus5.x
+            || rCactus3.x - 50 >= rCactus2.x || rCactus3.x - 50 >= rCactus1.x || rCactus3.x - 50 >= rCactus4.x || rCactus3.x - 50 >= rCactus5.x)
             {
                 int rand = (int)(Math.random()*4000+200);
                 rCactus3.x = WORLD_WIDTH+rand;
             }
 
-            if(rCactus4.x + 75 >= rCactus2.x || rCactus4.x + 75 >= rCactus3.x || rCactus4.x + 75 >= rCactus1.x || rCactus4.x + 75 >= rCactus5.x
-            || rCactus4.x - 75 >= rCactus2.x || rCactus4.x - 75 >= rCactus3.x || rCactus4.x - 75 >= rCactus1.x || rCactus4.x - 75 >= rCactus5.x)
+            if(rCactus4.x + 50 >= rCactus2.x || rCactus4.x + 50 >= rCactus3.x || rCactus4.x + 50 >= rCactus1.x || rCactus4.x +50 >= rCactus5.x
+            || rCactus4.x - 50 >= rCactus2.x || rCactus4.x - 50 >= rCactus3.x || rCactus4.x - 50 >= rCactus1.x || rCactus4.x - 50 >= rCactus5.x)
             {
                 int rand = (int)(Math.random()*4000+200);
                 rCactus4.x = WORLD_WIDTH+rand;
             }
 
-            if(rCactus5.x + 75 >= rCactus2.x || rCactus5.x + 75 >= rCactus3.x || rCactus5.x + 75 >= rCactus4.x || rCactus5.x + 75 >= rCactus1.x
-            || rCactus5.x - 75 >= rCactus2.x || rCactus5.x - 75 >= rCactus3.x || rCactus5.x - 75 >= rCactus4.x || rCactus5.x - 75 >= rCactus1.x)
+            if(rCactus5.x + 50 >= rCactus2.x || rCactus5.x + 50 >= rCactus3.x || rCactus5.x + 50 >= rCactus4.x || rCactus5.x + 50 >= rCactus1.x
+            || rCactus5.x - 50 >= rCactus2.x || rCactus5.x - 50 >= rCactus3.x || rCactus5.x - 50 >= rCactus4.x || rCactus5.x -50 >= rCactus1.x)
             {
                 int rand = (int)(Math.random()*4000+200);
                 rCactus5.x = WORLD_WIDTH+rand;
-            }
-
-            if(rBird.x + 75 >= rCactus1.x || rBird.x + 75 >= rCactus2.x || rBird.x + 75 >= rCactus3.x || rBird.x + 75 >= rCactus4.x || rBird.x + 75 >= rCactus5.x
-            || rBird.x - 75 >= rCactus1.x || rBird.x - 75 >= rCactus2.x || rBird.x - 75 >= rCactus3.x || rBird.x - 75 >= rCactus4.x || rBird.x - 75 >= rCactus5.x)
-            {
-                int rand = (int)(Math.random()*4000+200);
-                rBird.x = WORLD_WIDTH+rand;
             }
 
             if(hasCollided()){
@@ -396,7 +503,7 @@ public class Dino_Game extends ApplicationAdapter
     //Change the y and x of the rectangles to the top right instead of top left
     public boolean hasCollided(){
         if(dino.overlaps(rCactus1) || dino.overlaps(rCactus2) || dino.overlaps(rCactus3) || dino.overlaps(rCactus4) || dino.overlaps(rCactus5)
-        || dino.overlaps(rBird) || rMeteor.overlaps(ground)){
+        || dino.overlaps(rBird)){
             return true;
         }
         else{
